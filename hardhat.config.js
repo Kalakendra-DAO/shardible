4883ae22c46b9216345a81809fcb8caca2ae8c26
Immanuel-john <imjjohn18@gmail.com>
2022-09-28 15:31:34 +0530

    nft marketplace contract

---
+++ hardhat.config.js
@@ -0,0 +1,24 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+require("dotenv").config();
+const SHARDEUM_RPC = process.env.SHARDEUM_RPC;
+const privateKey = process.env.PRIVATE_KEY;
+
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  solidity: {
+    version: "0.8.10",
+  },
+  networks: {
+    shardeum: {
+      url: SHARDEUM_RPC,
+      accounts: [privateKey],
+      chainId: 8081,
+    }
+  },
+};
